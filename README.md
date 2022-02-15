a2171cdd2c26e29f4dd0e0ab760272e82a8cd1fa
Sajjad <sajjadmurtaza.nxb@gmail.com>
2022-02-15 10:27:00 +0100

    Enumerable test_in_order_of

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 * Enumerable#maximum/minimum
   * [Example](https://github.com/sajjadmurtaza/rails_updates_and_tips/blob/main/app/models/enumerable_updates.rb)
   * Rails 7 adds support for Enumerable#maximum and Enumerable#minimum to easily calculate the maximum and minimum value from a collection of enumerable elements.
+* Enumerable#test_in_order_of
+  * [Example](https://github.com/sajjadmurtaza/rails_updates_and_tips/blob/main/app/models/enumerable_updates.rb)
+  * by which we can order and constrain any enumerable collection using a key-series pair.
 
 ### Rails 6
--- app/models/enumerable_updates.rb
+++ app/models/enumerable_updates.rb
@@ -6,17 +6,33 @@ class EnumerableUpdates
   # to easily calculate the maximum and minimum value from a collection of enumerable elements.
 
   def self.test_maximum_minimum
-    student = Struct.new(:age)
-
-    students = [
-      student.new(19),
-      student.new(24),
-      student.new(33)
-    ]
+    students = new.send :students
 
     p students.maximum(:age) # 33
     p students.minimum(:age) # 19
   end
-end
+  # run EnumerableUpdates.test_maximum_minimum on console
 
-# run EnumerableUpdates.test_maximum_minimum on console
+  # ---------------------------------------------------- #
+
+  # Rails 7 introduces the Enumerable#in_order_of method,
+  # by which we can order and constrain any enumerable collection using a key-series pair.
+
+  def self.test_in_order_of
+    students = new.send :students
+
+    p students.in_order_of(:age, (15..25))
+  end
+
+  private
+
+  def students
+    student = Struct.new(:age)
+
+    [
+      student.new(19),
+      student.new(24),
+      student.new(33)
+    ]
+  end
+end
